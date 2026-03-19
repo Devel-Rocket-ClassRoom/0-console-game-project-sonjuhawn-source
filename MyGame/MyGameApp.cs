8f4456604c5e8db3c104789d6ee557fb03b2ba79
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-19 09:30:41 +0900

    프레임워크 복사완료

---
+++ MyGame/MyGameApp.cs
@@ -0,0 +1,24 @@
+﻿using Framework.Engine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Framework.MyGame
+{
+    public abstract class GameApp
+    {
+        protected GameApp(int width, int height);
+
+        protected ScreenBuffer Buffer { get; }
+
+        public event GameAction GameStarted;
+        public event GameAction GameStopped;
+
+        public void Run();
+        protected void Quit();
+
+        protected abstract void Initialize();
+        protected abstract void Update(float deltaTime);
+        protected abstract void Draw();
+    }
+}
---
+++ MyGame/TitleScene.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Framework.MyGame
+{
+    internal class TitleScene
+    {
+    }
+}
